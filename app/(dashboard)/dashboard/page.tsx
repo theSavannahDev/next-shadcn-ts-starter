d4bf9f5b11df82aabbedadd1d327edbe247e8a8d
Michael Owuor <michael.owuor@gmail.com>
2023-12-09 08:46:17 +0300

    chore: update project dependencies

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,3 @@
-import { Container } from "@/components/generic/container";
-
 const DashboardPage = () => {
   return <section>Dashboard Page</section>;
 };
---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Container } from "@/components/generic/container";
+
+const LandingPage = () => {
+  return (
+    <Container>
+      <section className="py-5">
+        <h1 className="font-montserrat text-4xl font-bold tracking-wide text-sky-800">
+          Your settings are well configured
+        </h1>
+        <p className="mt-5">
+          <Button asChild>
+            <Link href="/dashboard">Dashboard</Link>
+          </Button>
+        </p>
+      </section>
+    </Container>
+  );
+};
+
+export default LandingPage;
